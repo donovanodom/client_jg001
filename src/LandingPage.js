9b351ec2583ae457ed5bfa96e1cdcbe6f0487eb1
div <dodomdev@gmail.com>
2022-09-28 16:20:49 +0000

    Update 3 files

---
+++ src/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingPage = () => {
+    return (
+        <div className='landing-page'>
+            landingpage
+        </div>
+    )
+}
+
+export default LandingPage
